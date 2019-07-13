dae25a1ca3eb7838070af7405ce3bfd46792e0f9
SantiagoASC6 <sanosdu15@gmail.com>
2019-07-12 21:27:39 -0400

    good job

--- MASH.js
+++ MASH.js
@@ -1,10 +1,16 @@
 function mash() {
-    return ("You will live in a " + getHome() + ". You will marry " + spouse() + ", and you will have " + getChildrenCount() + " kids!" + ". You will drive a " + getCar())
+    let userName = process.argv[2]
+    if (userName == undefined) {
+        console.log("Type your name next to node MASH.js")
+    }
+    else {
+    return (process.argv[2] + ", You are a " + Career() + ", you will earn $" + Salary() + " a year" + ". You will live in a " + getHome() + ". You will marry " + spouse() + ", and you will have " + getChildrenCount() + " kids!" + ". You will drive a " + getCar())
+}
 }
 console.log(mash())
 
 function getHome() {
-    let homeChoice = process.argv[2]
+    let homeChoice = process.argv[3]
     let x = [homeChoice, "Mansion", "Apartment", "Shack", "House"];
     let house = Math.random();
     let rand = house * x.length;
@@ -24,7 +30,7 @@ function getChildrenCount () {
     let decimal = Math.random()
     let times100 = decimal * 100;
     let final = Math.floor(times100);
-    let Choice = process.argv[3]
+    let Choice = process.argv[4]
     if (Choice == undefined) {
         console.log("Enter desired number of children")
     }
@@ -41,7 +47,7 @@ function getChildrenCount () {
 //console.log(getChildrenCount());
 
 function getCar() {
-    let userCar = process.argv[4];
+    let userCar = process.argv[5];
     let x = [userCar,"lambo", "convertible", "box with wheels", "batmobile"];
     let car = Math.random();
     let randCar = car * x.length;
@@ -62,4 +68,19 @@ function spouse () {
     let integer = Math.floor(ranWife)
     let final = wife[integer]
     return final
+}
+function Career() {
+    let careerChoice = ["doctor", "astronaut", "plumer", "actor", "McDonalds employee"]
+    let job = Math.random();
+    let ranJob = job * careerChoice.length;
+    let integer = Math.floor(ranJob);
+    let final = careerChoice[integer];
+    return final;
+}
+
+function Salary() {
+    let amount = Math.random();
+    let increase = amount * 100000;
+    let final = Math.floor(increase);
+    return final;
 }
